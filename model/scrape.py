3e9ccef933c75e746d3069b22eef71fa1b6ccd61
Jay Esquivel <jaysqvl@gmail.com>
2022-12-03 19:47:47 -0800

    updated scraping dictionaries

--- model/scrape.py
+++ model/scrape.py
@@ -1,25 +1,33 @@
 import requests
-import enum
 
 from bs4 import BeautifulSoup
 
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-class ClothType(Enum):
-    JACKET = "jacket"
-    PANTS = "pants"
-    HAT = "hat"
-    SHOES = "shoes"
-
-
+# Soup (which contains the html) -> search the html for h1 -> 
 def find_cloth_type(soup):
+    search_for_me = soup.find("h1")[0].text.strip()
 
+    with open("./dictionaries/clothes", "r+") as f:
+        lines = f.readlines() # contains all the lines
+        f.seek(0)
+        count = 0
+        for l in lines:
+            if l in search_for_me:
+                if (count < 20 and count > 0):
+                    return "hat"
+                elif(count < 29 and count > 20):
+                    return "jacket"
+                elif(count < 38 and count > 29):
+                    return "pants"
+                elif(count < 49 and count > 38):
+                    return "shirt"
+                elif(count < 65 and count > 49):
+                    return "shoes"
+                elif(count > 65):
+                    return "sweatshirt"
+            count += 1
     return ""
+    
+
     
 def find_brand(soup):
     return ""
