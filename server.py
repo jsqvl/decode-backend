26064d7084680ae988d9d5d681fd1ad7b9e4d463
Jay Esquivel <jaysqvl@gmail.com>
2022-12-03 18:15:47 -0800

    Flask app routing

--- server.py
+++ server.py
@@ -1,12 +1,45 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 from model import calc, scrape
 
-app = Flask(__name__)
+import requests
+from flask_cors import CORS
 
-@app.route("/")
-def test():
-    return '1'
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+app = Flask(__name__)
+CORS(app)
+
+class Calc():
+    BLAH = "test"
+
+class Scraped():
+    BLAHH = "test"
+
+# immediately when you get to this domain
+@app.route('/webscrape')
+def getScrape():
+    # 
+    url_to_scrape = request.args.get('url')
+    blah_to_scrape = request.args.get('blah')
+    print(blah_to_scrape)
+    print(url_to_scrape)
+
+    return "test"
+
+@app.route('/')
+def getCalc():
+    
+    return "Calc"
+
+
+def main():
+    return "Hello World!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host = "0.0.0.0", port = 80)
